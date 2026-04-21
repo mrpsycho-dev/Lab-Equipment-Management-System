46b326610041bc20f2ce1d70b084e8e07d21c6f3
Muhammad Abdullah <muhammadabdullahnazir6@gmail.com>
2026-04-21 22:19:20 +0500

    initialize project and added data structures

---
+++ LEMS.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+
+int main()
+{
+
+  ////////////////////////////////////////////////////////////////
+  // EQUIPMENT DATA ARRAYS
+  ////////////////////////////////////////////////////////////////
+  int n = 200;
+  int equipmentCount = 5;
+  int equipmentId[n] = {1, 2, 3, 4, 5};
+  string equipmentName[n] = {"Microscope", "Oscilloscope", "BunsenBurner", "Voltmeter", "Centrifuge"};
+  string equipmentCat[n] = {"Biology\t", "Electronics", "Chemistry", "Electronics", "Biology\t"};
+  int equipmentQty[n] = {5, 3, 8, 6, 2};
+  int equipmentAvail[n] = {5, 3, 8, 6, 2};
+  string equipmentCond[n] = {"Good", "Good", "Fair", "Good", "Fair"};
+  float equipmentPrice[n] = {15000, 25000, 2000, 5000, 50000};
+  int nextEquipId = 6;
+
+  ////////////////////////////////////////////////////////////////
+  // BORROW DATA ARRAYS
+  ////////////////////////////////////////////////////////////////
+  int borrowCount = 0;
+  int borrowId[n];
+  string borrowEquipName[n];
+  string borrowerName[n];
+  string borrowerRoll[n];
+  string borrowDate[n];
+  string returnDate[n];
+  string borrowStatus[n];
+  string borrowIssuedBy[n];
+  int nextBorrowId = 1;
+
+  ////////////////////////////////////////////////////////////////
+  // HISTORY DATA ARRAYS
+  ////////////////////////////////////////////////////////////////
+  int historyCount = 0;
+  string historyAction[n];
+  string historyItem[n];
+  string historyBy[n];
+
+  ////////////////////////////////////////////////////////////////
+  // TECHNICIAN DATA ARRAYS
+  ////////////////////////////////////////////////////////////////
+  int techCount = 2;
+  string techUsername[10] = {"tech1", "tech2"};
+  string techName[10] = {"Dr.Sara", "Mr.Hamza"};
